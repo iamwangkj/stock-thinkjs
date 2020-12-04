5b1e631f9112c1f1c2f614533f2f99d5b68985b0
wangkj <wangkaijie5@jd.com>
2020-12-04 13:55:50 +0800

    feat: add crontab/girlImg

--- src/controller/crontab/girlImg.ts
+++ src/controller/crontab/girlImg.ts
@@ -3,26 +3,54 @@ import axios from 'axios'
 import cheerio from 'cheerio'
 
 export default class extends baseController {
+  // 获取每页的图片
+  async getList (pageIndex) {
+    // https://www.mzitu.com/zipai/comment-page-478/#comments
+    const resList = [] // 存放结果的数组
+    const url = `https://www.mzitu.com/zipai/comment-page-${pageIndex}/#comments`
+    const res = await axios.get(url)
+    const $ = cheerio.load(res.data)
+    $('.comment .comment-body').each((index, el) => {
+      const date = $(el).find('.comment-meta a').text().trim().replace('at ', '')
+      const imgUrl = $(el).find('p img.lazy').attr('data-original')
+      // console.log(imgUrl, date)
+      const item = { date, imgUrl }
+      resList.push(item)
+    })
+    return resList
+  }
+
+  sleep (ms:number) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms)
+    })
+  }
+
   async saveAction () {
     try {
-      const resList = [] // 存放结果的数组
       const url = 'https://www.mzitu.com/zipai/'
       const res = await axios.get(url)
       const $ = cheerio.load(res.data)
-      $('.comment .comment-body').each((index, el) => {
-        const date = $(el).find('.comment-meta a').text()
-        const imgUrl = $(el).find('p img.lazy').attr('data-original')
-        console.log('=', date, imgUrl)
-        const item = { date, imgUrl }
-        resList.push(item)
-      })
-      // const model = this.model('policy_for_gwy')
+      const pageTotal:number = $('.pagenavi-cm .page-numbers.current').text()
+      console.log('pageTotal=', pageTotal)
+
+      const model = this.mongo('girl_street')
+      let i = 1
+      let isEnd = false
+      while (!isEnd) {
+        const list = await this.getList(i)
+        const ids = await model.addMany(list)
+        console.log(`当前页码=${i}，插入条数=${ids.length}`, isEnd)
+        await this.sleep(3000)
+        i < pageTotal ? ++i : isEnd = true
+      }
+
+      // const model = this.mongo('girl_img')
       // let row = 0
       // let isEnd = false
       // let i = 0
       // while (!isEnd) {
-      //   const item = resList[i]
-      //   const { title, url } = item
+      //   const { title, url } = resList[i]
       //   const data = await model.where({ title }).find()
       //   if (think.isEmpty(data)) { // 如果数据库中没有找到该政策，即保存
       //     row = row + 1
@@ -34,7 +62,7 @@ export default class extends baseController {
       // }
       // return this.success(null, `insert ${row} row`)
     } catch (error) {
-      // return this.fail(-1, error.message)
+      return this.fail(-1, error.message)
     }
   }
 }
